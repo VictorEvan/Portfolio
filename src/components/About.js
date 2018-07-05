cefeca87ce60577ae62b1bbd0182072b3a416dde
Victor <victorevangelista@protonmail.com>
2018-07-05 03:23:43 -0700

    create About page

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return (
+      <section className="about">
+        <div className="about__about-half">
+        </div>
+        <div className="about__contact-half">
+        </div>
+      </section>
+    )
+  }
+}
+
+export default About;

49df34e35139fba177910834162832df68dd6351
Victor <victorevangelista@protonmail.com>
2018-07-06 09:19:07 -0700

    update hamburger button

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -2,15 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Hamburger = props => (
-  <button className="hamburger" onClick={()=> {props.toggleSideNav()}}>
-    <div className="line--top"></div>
-    <div className="line--middle"></div>
-    <div className="line--bottom"></div>
+  <button 
+    className={!props.navIsHidden ? 'hamburger is-active' : 'hamburger'}
+    onClick={()=> {props.toggleSideNav()}}
+  >
+    <span className="line"></span>
+    <span className="line"></span>
+    <span className="line"></span>
   </button>
 )
 
 Hamburger.propTypes = {
-  toggleSideNav: PropTypes.func.isRequired
+  toggleSideNav: PropTypes.func.isRequired,
+  sideNavIsOpen: PropTypes.bool.isRequired
 }
 
 export default Hamburger;

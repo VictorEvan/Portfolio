17e7c1f424bbca6aab127e9f098e4bac0d407f90
Victor <victorevangelista@protonmail.com>
2018-07-06 09:50:59 -0700

    add about half

--- src/components/About.js
+++ src/components/About.js
@@ -3,12 +3,19 @@ import React, { Component } from 'react';
 class About extends Component {
   render() {
     return (
-      <section className="about">
-        <div className="about__about-half">
-        </div>
-        <div className="about__contact-half">
-        </div>
-      </section>
+      <main className="about-contact">
+        <section className="about-contact__about-half">
+          <div className="contact-info">
+            <h2 className="contact-info__title">About Me</h2>
+            <h3 className="contact-info__description">Front End Engineer</h3>
+            <p className="contact-info__paragraph">
+              I'm Victor Evangelista, a Web Developer focused on Front End Engineering. I make cool things with the latest technologies the ever fast changing web has to offer. I love to create smart and dynamic user interfaces, develop rich web experiences, and think creatively to solve design problems. Available for hire.
+            </p>
+          </div>
+        </section>
+        <section className="about-contact__contact-half">
+        </section>
+      </main>
     )
   }
 }

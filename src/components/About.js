17ab33586794f5b5be5cb6fb89ac6c6946ee355b
Victor <victorevangelista@protonmail.com>
2018-07-08 19:51:12 -0700

    update about/contact component and styles

--- src/components/About.js
+++ src/components/About.js
@@ -14,6 +14,29 @@ class About extends Component {
           </div>
         </section>
         <section className="about-contact__contact-half">
+          <div className="contact-info">
+            <h2 className="contact-info__title">Contact Info</h2>
+            <h3 className="contact-info__description">Let's Talk</h3>
+            <p className="contact-info__paragraph">Email: victorevangelista@protonmail.com</p>
+            <p className="contact-info__paragraph">Phone: 725-696-1583</p>
+            <ul className="contact-info__list">
+              <li className="social-link-container">
+                <a className="social-link" target="_blank" rel="noopener noreferrer" href="https://www.github.com/victorevan">
+                  <img className="social-link__image--github" src={require('../images/social/github.png')} alt="GitHub"/>
+                </a>
+              </li>
+              <li className="social-link-container">
+                <a className="social-link" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/victorevan">
+                  <img className="social-link__image--linkedin" src={require('../images/social/linkedin.png')} alt="Linkedin"/>
+                </a>
+              </li>
+              <li className="social-link-container">
+                <a className="social-link" target="_blank" rel="noopener noreferrer" href="https://www.codepen.io/victorevangelista">
+                  <img className="social-link__image--codepen" src={require('../images/social/codepen.png')} alt=""/>
+                </a>
+              </li>
+            </ul>
+          </div>
         </section>
       </main>
     )

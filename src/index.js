d7e1ebc49b55f5dd4c3096b92f34927d5a9b87f7
Victor <victorevangelista@protonmail.com>
2018-07-10 11:15:39 -0700

    update Router for static file delivery

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom'; 
 import App from './App';
-import { BrowserRouter, Route } from 'react-router-dom'; 
+import { Route, HashRouter } from 'react-router-dom'; 
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter basename={process.env.PUBLIC_URL}>
     <Route render={props => (
       <App {...props} />
     )}/>
-  </BrowserRouter>
+  </HashRouter>
 , document.querySelector('[react-js="root"]'));
 
 registerServiceWorker();

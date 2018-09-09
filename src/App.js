dae2481fe1ca3f73f23b7913b0699a68f2ca9c8b
Victor <victorevangelista@protonmail.com>
2018-09-08 20:56:50 -0700

    modularize projects data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom'; 
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import './scss/App.css';
+import projects from './data/projects'
 
 import Header from './components/Header';
 import Intro from './components/Intro';
@@ -435,100 +436,7 @@ App.defaultProps = {
     '/projects/random-quote-machine': true,
     '/projects/pomodoro-clock': true
   },
-  projects: [
-    {
-      title: 'tic-tac-toe',
-      description: 'Single or Multiplayer Redux Game',
-      programmingDescription: "React's reusable presentational components and Redux' store for app state proved to make a delightful cocktail for this game.",
-      programmingLink: {
-        href: "https://github.com/VictorEvan/Tic-Tac-Toe/blob/master/src/redux/redux.js",
-        text: "Redux Source Code"
-      },
-      programmingLinkDescription: "Take a deeper look into the state and logic behind the app. Action Type Creators, Reducers, a MiniMax algorithm- Oh my!",
-      designDescription: "Using Sass as my CSS preprocessor proved to be quintessential in rapidly developing quality styles with variables, mixins, and extends.",
-      designLink: {
-        href: "https://github.com/VictorEvan/Tic-Tac-Toe/blob/master/src/scss/layout/_layout.scss",
-        text: "Layout Source Code"
-      },
-      designLinkDescription: "Check out how CSS Flex, Grid, and Position are used to create the game layout. Also noteworthy- Sass extends!",
-      thoughtsDescription: "Creating this application from scratch taught me a lot about how React lifecycle methods can be used in conjunction with Redux.",
-      notableSkillsUsed: [
-        "React Component Lifecycle Methods",
-        "React CSS Transitions",
-        "Redux State Management",
-        "Sass CSS Preprocessor",
-      ],
-    },
-    {
-      title: 'calculator',
-      description: 'Fully functional & responsive',
-      programmingDescription: "Using React for this application was a pleasure. The majority of the UI is dynamically rendered by reusable React Components.",
-      programmingLink: {
-        href: "https://github.com/VictorEvan/Calculator/blob/master/src/App.js",
-        text: "App Source Code"
-      },
-      programmingLinkDescription: "Take a look at the building blocks and logic behind the app, all following industry patterns.",
-      designDescription: "Inspired by the UI of the Android calculator app, I sought to create my own fullscreen responsive design using CSS Grid and Flex.",
-      designLink: {
-        href: "https://github.com/VictorEvan/Calculator/blob/master/src/scss/_layout.scss",
-        text: "Layout Source Code"
-      },
-      designLinkDescription: "Take a look at the short, simple, and beautiful layout rules that make the app just work.",
-      thoughtsDescription: "This made for a fun project to practice programmatically generating UI elements each with their own callback functions built in.",
-      notableSkillsUsed: [
-        "CSS Grid/Flexbox",
-        "Dynamically Rendered UI Components",
-        "JavaScript Closures",
-        "Edge Case Handling",
-      ],
-    },
-    {
-      title: 'random-quote-machine',
-      description: 'Inspirational quotes from the web',
-      programmingDescription: "All the quotes behind this beautiful app are pulled from the web using the fetch API, and can be easily be shared on Twitter.",
-      programmingLink: {
-        href: "https://github.com/VictorEvan/Random-Quote-Machine/blob/master/src/App.js",
-        text: "App Source Code"
-      },
-      programmingLinkDescription: "Take a look behind the scenes at the built in methods that handle changing the app state.",
-      designDescription: "The design for this app was greatly inspired by quotefancy who combine beautiful background images with fantastic quotes.",
-      designLink: {
-        href: "https://github.com/VictorEvan/Random-Quote-Machine/blob/master/src/scss/App.scss",
-        text: "Styles Source Code"
-      },
-      designLinkDescription: "Check out the Sass mixins, lists and control directives that bring the app to life!",
-      thoughtsDescription: "This was a highly enjoyable and uplifting project to work on. I love designing and coding beautifully simple apps.",
-      notableSkillsUsed: [
-        "JavaScript fetch API",
-        "Sass Mixins, Lists & Control Directives",
-        "Image Caching",
-        "Regex Data Cleansing",
-      ],
-    },
-    {
-      title: 'pomodoro-clock',
-      description: 'Simple productivity app',
-      programmingDescription: "There has to be something interesting I can say about a clock.",
-      programmingLink: {
-        href: "https://github.com/VictorEvan/Pomodoro-Clock/blob/master/src/App.js",
-        text: "App Source Code"
-      },
-      programmingLinkDescription: "Take a look at the accurate time tracking implementation.",
-      designDescription: "Creating the layout for this app was fast and easy with Flexbox.",
-      designLink: {
-        href: "https://github.com/VictorEvan/Pomodoro-Clock/blob/master/src/scss/_layout.scss",
-        text: "Layout Source Code"
-      },
-      designLinkDescription: "Flex Mania",
-      thoughtsDescription: "The accurate timer for this app will prove useful for future projects.",
-      notableSkillsUsed: [
-        "Dynamically Rendered Components",
-        "Responsible State Handling",
-        "JavaScript Closures",
-        "Flexbox",
-      ]
-    }
-  ]
+  projects
 };
 
 export default App;

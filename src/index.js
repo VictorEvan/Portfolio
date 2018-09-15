a05c65aa0ca78235e326ea120632f42b1a37f614
Victor <victorevangelista@protonmail.com>
2018-09-15 16:37:50 -0700

    implement historyObject

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom'; 
 import App from './App';
-import { Route, BrowserRouter } from 'react-router-dom'; 
+import { Router, Route } from 'react-router-dom'; 
 import registerServiceWorker from './registerServiceWorker';
+import { createBrowserHistory } from 'history';
+import historyTracker from './helper/historyTracker';
+
+const customHistory = createBrowserHistory({basename: process.env.basename});
+const addHistory = historyTracker();
+let historyObject = { from: null, to: null };
+
+customHistory.listen((location) => {
+  historyObject = addHistory(location);
+});
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Route render={props => (
-      <App {...props} />
-    )}/>
-  </BrowserRouter>
+  <Router history={customHistory} >
+    <Route render={(props) => <App {...props} addHistory={addHistory} historyObject={historyObject} />} />
+  </Router>
 , document.querySelector('[react-js="root"]'));
 
 registerServiceWorker();

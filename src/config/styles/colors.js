1fd077aab302f69b9c4b2bd92a7143f2c78f8410
Victor <victorevangelista@protonmail.com>
2018-09-16 22:30:31 -0700

    create colors config

---
+++ src/config/styles/colors.js
@@ -0,0 +1,13 @@
+const resumeDarkBlue = "#313638";
+const resumeLightBlue = "#5271ff";
+const white = "#FFF";
+
+const lightThemeColors = {
+  main: resumeDarkBlue,
+  secondary: resumeLightBlue,
+  tertiary: white
+};
+
+const darkThemeColors = {
+
+}
